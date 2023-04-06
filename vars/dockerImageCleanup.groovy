5b6d239679e526a4e879d8e1ace69c7ccafc1562
abdulrahman911 <rahmanuk91@gmail.com>
2023-04-06 13:51:41 +0530

    fixed ecr image cleanup

--- vars/dockerImageCleanup.groovy
+++ vars/dockerImageCleanup.groovy
@@ -7,7 +7,6 @@
 
 def call(String aws_account_id, String region, String ecr_repo_name){
    sh """
-     // docker build -t 200955153941.dkr.ecr.ap-south-1.amazonaws.com/myjava-app:latest .
      docker rmi ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repo_name}:latest
    """  
 }
