a805bf22807a6f02f50fc450a6b5d410b7f126db
abdulrahman911 <rahmanuk91@gmail.com>
2023-04-06 10:15:50 +0530

    added docker scan with trivy, fixed syntax error

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,6 +1,6 @@
 def call(String dockerHubUser, String project, String imageTag){
     sh """
-    trivy image ${dockerHubUser}/${project} ${dockerHubUser}/${project}:latest > scan.txt
+    trivy image ${dockerHubUser}/${project}:latest > scan.txt
     cat scan.txt
     """
 }
