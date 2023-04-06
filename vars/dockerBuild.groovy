18ab051b0de12956e0e1472a8ccf037f8cf44ef2
abdulrahman911 <rahmanuk91@gmail.com>
2023-04-06 13:38:17 +0530

    fixed ecr image build

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -6,12 +6,12 @@
 //     docker image tag ${dockerHubUser}/${project} ${dockerHubUser}/${project}:latest
 //     """
 // }
+// docker build -t 200955153941.dkr.ecr.ap-south-1.amazonaws.com/myjava-app:latest .
+//docker tag myjava-app:latest 200955153941.dkr.ecr.ap-south-1.amazonaws.com/myjava-app:latest
 
-def call(String aws_account_id, String region, String ecr_repo_name){
+def call(String aws_account_id, String region, String ecr_repo_name){  
    sh """
-     // docker build -t 200955153941.dkr.ecr.ap-south-1.amazonaws.com/myjava-app:latest .
      docker build -t ${ecr_repo_name} .
-     //docker tag myjava-app:latest 200955153941.dkr.ecr.ap-south-1.amazonaws.com/myjava-app:latest
      docker tag ${ecr_repo_name}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repo_name}:latest .
    """  
 }
